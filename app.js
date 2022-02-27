bd27b36b59e8592a2d6438953a67b7e55582ee10
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-02-27 13:06:33 -0700

    nav

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 
 function hamburgerMenu() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
 
-  

accc6e64bd316e2ac64787792a1e9b4f0f9cff39
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-02-28 15:43:46 -0700

    contact progress

---
+++ app2.js
@@ -0,0 +1,10 @@
+
+function validation() {
+    let name = document.getElementById("name").value;
+    let subject = document.getElementById("subject").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let errorMessage = document.getElementById("error-message");
+
+  }

e8abdb4a352d7ec3af0cb131a62230ac6927a0e3
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-02-28 15:47:39 -0700

    contact

--- contact.js
+++ contact.js
@@ -6,5 +6,14 @@ function validation() {
     let email = document.getElementById("email").value;
     let message = document.getElementById("message").value;
     let errorMessage = document.getElementById("error-message");
+    let text;
 
+    errorMessage.style.padding = ".8em";
+
+    if(name.length < 5){
+        text = "Please Enter Vaild Name";
+        errorMessage.innerHTML = text;
+        return false;
+    }
+    return false
   }

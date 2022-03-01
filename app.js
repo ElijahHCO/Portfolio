a3471862e3ce57dfd80876609e8f6cf5cd6a1c4e
ElijahHCO <93837790+ElijahHCO@users.noreply.github.com>
2022-03-01 10:47:13 -0700

    progress check

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
+$(document).ready(() => {
+  var links = $("#myLinks");
+  var hamburger = $("#hamburger");
 
-function hamburgerMenu() {
-  var x = document.getElementById("links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+  hamburger.click(() => {
+    if (links.css("display") === "block") {
+      links.css("display", "none");
+    } else {
+      links.css("display", "block");
+    }
+  });
+
+
+  const $popUp = $(`#pop-up`);
+  const $closeBtn = $(`#close`);
+  const modalOpened = localStorage.getItem("modalOpened");
+
+  const openModal = () => {
+    $popUp.css('display', 'block')
   }
-}
+  const closeModal = () => {
+    $popUp.css('display', 'none');
+    localStorage.setItem("modalOpened");
+  }
+  $closeBtn.on('click', closeModal);
 
-$(
-  () => {
-  
-    const $popUp = $(`#pop-up`);
-    const $closeBtn = $(`#close`);
-
-    const openModal = () => {
-      $popUp.css('display', 'block')
-    }
-    const closeModal = () => {
-      $popUp.css('display', 'none');
-    }
-    $closeBtn.on('click', closeModal);
-
+  if(!modalOpened) {
     setTimeout(openModal, 3000)
   }
-)
+})
--- contact.js
+++ contact.js
@@ -1,6 +1,7 @@
+$("#submitBtn").click(submitEmail)
 
 function submitEmail() {
-    let name = document.getElementById("name").value;
+    let name = $("#name").val();
     let subject = document.getElementById("subject").value;
     let phone = document.getElementById("phone").value;
     let email = document.getElementById("email").value;
@@ -10,42 +11,40 @@ function submitEmail() {
 
     errorMessage.style.padding = ".8em";
 
-    if(name.length < 5){
+    if (name.length < 5) {
         text = "Please Enter a Name";
         errorMessage.innerHTML = text;
         return false;
     }
 
-    if(subject.length < 5){
+    if (subject.length < 5) {
         text = "Please Enter a Subject";
         errorMessage.innerHTML = text;
         return false;
     }
 
-    if(isNaN(phone) || phone.length < 9){
+    if (isNaN(phone) || phone.length < 9) {
         text = "Please Enter a Valid Phone #";
         errorMessage.innerHTML = text;
         return false;
     }
 
-    if(email.indexOf("@") == -1 || email.length < 6){
+    if (email.indexOf("@") == -1 || email.length < 6) {
         text = "Please Enter a Vaild Email";
         errorMessage.innerHTML = text;
         return false;
     }
 
-    if(message.length <= 100){
+    if (message.length <= 10) {
         text = "Message must be more than 100 characters";
         errorMessage.innerHTML = text;
         return false;
     }
-    alert(`Thank you, ${name}! I look forward to speaking with you!`)
-    return true;
-    
 
-  }
- 
- 
-  
-  
-  
+    window.open(`mailto:hurnelijah@yahoo.com?subject=${subject}&body=${name}%0A${message}%0A${phone}`)
+    // alert(`Thank you, ${name}! I look forward to speaking with you!`);
+
+    return true;
+}
+
+
